ab07482359d4ca1419fce8fa23ec2d59e91eef41
Brad P <BradPatras@users.noreply.github.com>
2025-01-22 22:05:59 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "git@github.com:tutu-ru-mobile/MBProgressHUD.git", exact: "1000.0.0"),
+        .package(url: "git@github.com:buildertrend/MBProgressHUD.git", exact: "1000.0.0"),
         .package(url: "git@github.com:SDWebImage/SDWebImage.git", from: "5.19.0"),
     ],
     targets: [
